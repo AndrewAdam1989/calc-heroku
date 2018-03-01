38da369e47bb429d2b58388ee8a1bf8d2618924a
AndrewAdam1989 <36536198+AndrewAdam1989@users.noreply.github.com>
2018-03-01 07:04:22 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
-cd /v2raybin
+cd /cbin
 echo -e -n "$CONFIG_JSON1" > config.json
 echo -e -n "$PORT" >> config.json
 echo -e -n "$CONFIG_JSON2" >> config.json
 echo -e -n "$UUID" >> config.json
 echo -e -n "$CONFIG_JSON3" >> config.json
+echo -e -n "$ALTERID" >> config.json
+echo -e -n "$CONFIG_JSON4" >> config.json
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-echo -e "$CERT_PEM" > cert.pem
-echo -e "$KEY_PEM"  > key.pem
+echo -e "$CERT_PEM" > c.pem
+echo -e "$KEY_PEM"  > k.pem
 fi
-./v2ray
+./calc
